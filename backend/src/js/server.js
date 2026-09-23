61f9ef060a494018d1c2c4a35dfb489683e1a88a
Kelyan Sanches Gomes <kelyan.sanches-gomes@etu.univ-cotedazur.fr>
2026-09-23 18:40:21 +0200

    server js backend  updated still things to do tho

--- backend/src/js/server.js
+++ backend/src/js/server.js
@@ -30,4 +30,49 @@ const connectWithRetry = () => {
 };
 connectWithRetry();
 
-//TODO API THINGS
+//API
+
+app.get('/api/weather', async (req, res) => {
+  const city = req.query.city;
+  if (!city) {
+    return res.status(400).json({"A city is required" });
+  }
+
+  try {
+    //with that i can get the lat and lon of the city fetched
+    const geoRes = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}&count=1&language=fr&format=json`);
+    const geoData = await geoRes.json();
+
+    if (!geoData.results || geoData.results.length === 0) {return res.status(404).json({ error: 'City not found' });}
+
+    const location = geoData.results[0];
+    const { latitude, longitude, name, country } = location;
+    //fetch the meteo datas
+    const weatherRes = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
+    const weatherData = await weatherRes.json();
+
+    res.json({
+      cityName: name,
+      country: country || '',
+      latitude,
+      longitude,
+      temperature: weatherData.current_weather.temperature,
+      windspeed: weatherData.current_weather.windspeed,
+      weathercode: weatherData.current_weather.weathercode,
+      time: weatherData.current_weather.time
+    });
+  }///catch (error){...}
+
+  //}
+
+  //fav cities
+  app.get('/api/favorites', async (req, res) => {
+    const favorites = await Favorite.find().sort({ addedAt: -1 });
+    res.json(favorites);
+  }//TODO catch error (trycatch)
+
+  //TODO Sav the city in the fav
+
+  app.listen(PORT, () => {
+  console.log(`Backend Weather API on ${PORT} port`);//to make sure
+});
